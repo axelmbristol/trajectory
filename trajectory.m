9c22ac10f8f916b8b3151e8746440fe50f197771
Axel <axelmontout@hotmail.fr>
2018-10-12 04:34:09 +0100

    plot from db is working.

--- trajectory.m
+++ trajectory.m
@@ -1,30 +1,55 @@
 fprintf('start...\n');
-
 fprintf('connecting to mongoDB...\n');
-
-
-javaaddpath('C:\Users\%USER\Documents\MATLAB\JAR\mongo-java-driver-3.4.2.jar')
-% import the library
+javaaddpath('C:\Users\Axel\Documents\MATLAB\JAR\mongo-java-driver-3.8.2.jar')
 import com.mongodb.*;
 
-% To directly connect to a single MongoDB server (note that this will not auto-discover the primary even
-% if it's a member of a replica set:
 mongoClient = MongoClient();
-% db object will be a connection to a MongoDB server for the specified database
-db = mongoClient.getDB( 'mydb' );
-% get the list of collections
-colls = db.getCollectionNames();
-% get a collection
-coll = db.getCollection('testCollection');
+db = mongoClient.getDB( 'main' );
+fprintf('find collections...\n');
+colls = db.getCollectionNames().toArray();
+disp(colls);
 
+currColl = "_70101100019-1/15/15";
+disp("currColl="+currColl);
 
+coll = db.getCollection(currColl).findOne().toString().toCharArray;
+fprintf('coll=\n');
+disp(size(coll))
 
+data = reshape(coll,[1,size(coll)]);
 
+fprintf('json decode...\n');
+json = jsondecode(data);
+fprintf('json=\n');
+disp(json);
 
-conn = mongo("localhost",27017,"main");
-isopen(conn);
+fprintf('json ok\n');
 
-x = [1; 2; 3];
-y = [1; 2; 3];
-z = [1; 2; 3];
-scatter3(x,y,z,'filled')
+fprintf('start parsing...\n');
+
+animalArray = json.animals;
+
+xmin = [];
+ymin = [];
+zmin = [];
+xmax = [];
+ymax = [];
+zmax = [];
+for i = 1:size(animalArray)
+   tagDataArray = animalArray(i).tagData;
+   for j = 1:size(tagDataArray)
+        tag = tagDataArray(j);
+        raw = tag.second_sensor_values_xyz;
+        split = strsplit(raw,':');
+        xmin = [xmin,str2double(split(1))];
+        ymin = [ymin,str2double(split(3))];
+        zmin = [zmin,str2double(split(5))];
+        xmax = [xmax,str2double(split(2))];
+        ymax = [ymax,str2double(split(4))];
+        zmax = [zmax,str2double(split(6))];
+   end
+end
+fprintf('finished parsing...\n');
+fprintf('scatter3...\n');
+scatter3(xmin,ymin,zmin,'filled')
+%scatter3(xmax,ymax,zmax,'filled')
