86cb5847918a634aebaf96036c14069aaf0f298c
Axel <axelmontout@hotmail.fr>
2018-10-12 17:12:17 +0100

    plot acceleration for each animal over time.

--- trajectory.m
+++ trajectory.m
@@ -29,27 +29,59 @@ fprintf('start parsing...\n');
 
 animalArray = json.animals;
 
-xmin = [];
-ymin = [];
-zmin = [];
-xmax = [];
-ymax = [];
-zmax = [];
+xmin = 0;
+ymin = 0;
+zmin = 0;
+xmax = 0;
+ymax = 0;
+zmax = 0;
+gravity = [0, 0, 0];
+linear_acceleration = [0, 0, 0];
+
 for i = 1:size(animalArray)
    tagDataArray = animalArray(i).tagData;
+   x = [];
+   y = [];
+   z = [];
+   time = [];
+   name = "unknown";
    for j = 1:size(tagDataArray)
         tag = tagDataArray(j);
         raw = tag.second_sensor_values_xyz;
+        name = tag.serial_number.x_numberLong;
         split = strsplit(raw,':');
-        xmin = [xmin,str2double(split(1))];
-        ymin = [ymin,str2double(split(3))];
-        zmin = [zmin,str2double(split(5))];
-        xmax = [xmax,str2double(split(2))];
-        ymax = [ymax,str2double(split(4))];
-        zmax = [zmax,str2double(split(6))];
+        xmin = str2double(split(1));
+        ymin = str2double(split(3));
+        zmin = str2double(split(5));
+        xmax = str2double(split(2));
+        ymax = str2double(split(4));
+        zmax = str2double(split(6));
+        aplha = 0.8;
+        gravity(1) = aplha * gravity(1) + (1 - aplha)*xmin;
+        gravity(2) = alpha * gravity(2) + (1 - aplha)*ymin;
+        gravity(3) = alpha * gravity(3) + (1 - aplha)*zmin;
+        
+        linear_acceleration(1) = xmin - gravity(1);
+        linear_acceleration(2) = ymin - gravity(2);
+        linear_acceleration(3) = zmin - gravity(3);
+        
+        x = [x, linear_acceleration(1)];
+        y = [y, linear_acceleration(2)];
+        z = [z, linear_acceleration(3)];
+        time = [time, j];
    end
+   disp(name);
+   N = size(animalArray);
+   W = floor(sqrt(N));
+   width = W(1)+1;
+   height = ceil(N/W);
+
+   disp(i);
+   subplot(width,height,i);
+   plot(time,x,time,y,time,z);
+   title(name);
+   
+   
 end
-fprintf('finished parsing...\n');
-fprintf('scatter3...\n');
-scatter3(xmin,ymin,zmin,'filled')
+
 %scatter3(xmax,ymax,zmax,'filled')
